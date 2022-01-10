04519754b8f4259d031dc82bebfa9c621d386735
A200003514 <milan38916>
2022-01-10 18:09:59 +0100

    openApi addUser

--- src/main/java/com/backend/java_swagger_open_api/controller/UsersController.java
+++ src/main/java/com/backend/java_swagger_open_api/controller/UsersController.java
@@ -6,10 +6,8 @@ import com.backend.java_swagger_open_api.repository.UsersActions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -18,25 +16,19 @@ public class UsersController implements UsersApi {
     @Autowired
     UsersActions usersActions;
 
-
-
-    @GetMapping(value = "/user/{username}")
-    public ResponseEntity<User> getAllUsers(@PathVariable String username) {
-        return new ResponseEntity<>(usersActions.getUser(username), HttpStatus.OK);
-    }
-
-    @PostMapping(value = "/addUser")
-    public ResponseEntity<String> addNewUser(@Valid @RequestBody User user, Errors errors) {
-        if (!errors.hasErrors()) {
-            usersActions.addUser(user);
-            return new ResponseEntity<>("User was added successfully", HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>("Error: " + errors.getAllErrors().get(0).getDefaultMessage(), HttpStatus.BAD_REQUEST);
-        }
-    }
-
     @Override
     public ResponseEntity<List<User>> getAllUsers() {
         return new ResponseEntity<>(usersActions.getUsers(), HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity<User> getOneUser(String username) {
+        return new ResponseEntity<>(usersActions.getUser(username), HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity<String> addOneUser(User user) {
+            usersActions.addUser(user);
+            return new ResponseEntity<>("User was added successfully", HttpStatus.OK);
     }
 }
