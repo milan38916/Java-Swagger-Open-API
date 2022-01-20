f3c2d924bbfbfa16426111eb5c4d302cde84cd58
Marcinek <Milan.Marcinek@t-systems.com>
2022-01-20 17:02:48 +0100

    update

--- src/main/java/com/backend/java_swagger_open_api/controller/BooksController.java
+++ src/main/java/com/backend/java_swagger_open_api/controller/BooksController.java
@@ -6,12 +6,14 @@ import com.backend.java_swagger_open_api.repository.BooksActions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 public class BooksController implements BooksApi {
 
     @Autowired
